144e9429c2428a5fbde27ff9a5071af711ca55ad
Ajinkya Ganesh Kumbhar <ajinkyakumbhar423@gmail.com>
2024-09-29 00:11:58 +0530

    Update api.go

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -56,6 +56,7 @@ func (n *api) Send(obj map[string]interface{}, templates []string, dest services
 	}
 
 	vars := n.getVars(obj, dest)
+	fmt.Printf("vars: %v", vars)
 
 	in := make(map[string]interface{})
 	for k := range vars {
